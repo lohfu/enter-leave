5f9d6500a4aa62125bef75c2f4d0d433c59dbc84
Linus Miller <lohfu@lohfu.io>
2016-04-23 16:38:16 +0200

    Retab.

--- src/enter-leave.js
+++ src/enter-leave.js
@@ -5,143 +5,143 @@ var transitionend = 'transitionend';
 var fxq = require('fxq');
 
 document.addEventListener(transitionend, function(e) {
-	if (e.target.matches('.animate.active')) {
-		e.stopPropagation();
-		fxq.dequeue(e.target);
-	}
+  if (e.target.matches('.animate.active')) {
+    e.stopPropagation();
+    fxq.dequeue(e.target);
+  }
 });
 
 function transition(className, options, complete) {
-	options = options || {};
+  options = options || {};
 
-	// TODO perhaps className should be an array of classes
-	if (options.className)
-		className += ' ' + options.className;
+  // TODO perhaps className should be an array of classes
+  if (options.className)
+    className += ' ' + options.className;
 
-	var stop;
+  var stop;
 
-	return {
-		start: function(next, hooks) {
-			hooks.stop = stop = animate.call(this, className, options);
-		},
+  return {
+    start: function(next, hooks) {
+      hooks.stop = stop = animate.call(this, className, options);
+    },
 
-		finish: function() {
-			if (complete) complete.call(this);
+    finish: function() {
+      if (complete) complete.call(this);
 
-			stop();
+      stop();
 
-			this.classList.remove('animate', 'active');
-			this.style.height = '';
-			
-			fxq.dequeue(this);
-		}
-	};
+      this.classList.remove('animate', 'active');
+      this.style.height = '';
+      
+      fxq.dequeue(this);
+    }
+  };
 }
 
 function animate(className, options) {
-	var self = this;
+  var self = this;
 
-	this.classList.add.apply(this.classList, className.split(' ').concat('animate'));
+  this.classList.add.apply(this.classList, className.split(' ').concat('animate'));
 
-	// force redraw
-	this.offsetHeight;
+  // force redraw
+  this.offsetHeight;
 
-	var timeout = setTimeout(function() {
-		self.classList.add('active');
-		//self.addClass('active');
+  var timeout = setTimeout(function() {
+    self.classList.add('active');
+    //self.addClass('active');
 
-		var duration = parseFloat(window.getComputedStyle(self).getPropertyValue('transition-duration'));
+    var duration = parseFloat(window.getComputedStyle(self).getPropertyValue('transition-duration'));
 
-		timeout = setTimeout(function() {
-			// finish animation if we are still waiting for transitionend
+    timeout = setTimeout(function() {
+      // finish animation if we are still waiting for transitionend
 
-			//if (elem.is('.animate.active'))
-			if (self.matches('.animate.active'))
-				fxq.dequeue(self);
-		}, duration > 0 ? duration * 1100 : 0);
-	});
+      //if (elem.is('.animate.active'))
+      if (self.matches('.animate.active'))
+        fxq.dequeue(self);
+    }, duration > 0 ? duration * 1100 : 0);
+  });
 
-	return function() {
-		clearTimeout(timeout);
-		// TODO maybe split(' ') and apply?
-		self.classList.remove.apply(self.classList, className.split(' '));
-		//elem.removeClass(className);
-	};
+  return function() {
+    clearTimeout(timeout);
+    // TODO maybe split(' ') and apply?
+    self.classList.remove.apply(self.classList, className.split(' '));
+    //elem.removeClass(className);
+  };
 }
 
 module.exports = {
 
-	toggle: function(options) {
-		if(this.hasClass('hidden') || this.hasClass('leave'))
-			this.show(options);
-		else 
-			this.hide(options);
-	},
+  toggle: function(options) {
+    if(this.hasClass('hidden') || this.hasClass('leave'))
+      this.show(options);
+    else 
+      this.hide(options);
+  },
 
-	enter: function(element, targetElement, options, complete) {
-		options = options || {};
+  enter: function(element, targetElement, options, complete) {
+    options = options || {};
 
-		var enter = transition('enter', options, complete);
+    var enter = transition('enter', options, complete);
 
-		fxq.queue(element, function() {
-			// TODO implement different insertiong methods
-			targetElement.appendChild(element);
+    fxq.queue(element, function() {
+      // TODO implement different insertiong methods
+      targetElement.appendChild(element);
 
-			element.classList.remove('hidden');
+      element.classList.remove('hidden');
 
-			if (options.animateHeight)
-				element.style.height = element.scrollHeight;
+      if (options.animateHeight)
+        element.style.height = element.scrollHeight;
 
-			enter.start.apply(element, arguments);
-		});
-		
-		fxq.queue(element, enter.finish);
-	},
+      enter.start.apply(element, arguments);
+    });
+    
+    fxq.queue(element, enter.finish);
+  },
 
-	leave: function(element, options, complete) {
+  leave: function(element, options, complete) {
 
-		var leave = transition('leave', options, complete || function() {
-			// remove element when transition ends
-			element.parentNode.removeChild(element);
-		});
+    var leave = transition('leave', options, complete || function() {
+      // remove element when transition ends
+      element.parentNode.removeChild(element);
+    });
 
-		fxq.finish(element);
+    fxq.finish(element);
 
-		if (options && options.animateHeight && !element.classList.contains('animate')) {
-			fxq.queue(element, function(next, hooks) {
-				element.style.height = element.scrollHeight;
+    if (options && options.animateHeight && !element.classList.contains('animate')) {
+      fxq.queue(element, function(next, hooks) {
+        element.style.height = element.scrollHeight;
 
-				// force redraw
-				element.offsetHeight;
+        // force redraw
+        element.offsetHeight;
 
-				var timeout = setTimeout(next);
+        var timeout = setTimeout(next);
 
-				hooks.stop = function() {
-					clearTimeout(timeout);
-				};
-			});
-		}
-		
-		fxq.queue(element, leave.start);
-		fxq.queue(element, leave.finish);
-	},
+        hooks.stop = function() {
+          clearTimeout(timeout);
+        };
+      });
+    }
+    
+    fxq.queue(element, leave.start);
+    fxq.queue(element, leave.finish);
+  },
 
-	show: function(element, options) {
-		fxq.finish(element);
+  show: function(element, options) {
+    fxq.finish(element);
 
-		fxq.queue(element, function(next) {
-			this.classList.add('visible');
+    fxq.queue(element, function(next) {
+      this.classList.add('visible');
 
-			next();
-		});
+      next();
+    });
 
-		this.enter(null, options);
-	},
+    this.enter(null, options);
+  },
 
-	hide: function(element, options) {
-		this.leave(element, options, function() {
-			this.classList.add('hidden');
-			this.classList.remove('visible');
-		});
-	}
+  hide: function(element, options) {
+    this.leave(element, options, function() {
+      this.classList.add('hidden');
+      this.classList.remove('visible');
+    });
+  }
 };
